286b1f4cc257f90330ecd1b13d37f78b79ba7daa
Jeshwin William James <104018590+jeshwinwilliam@users.noreply.github.com>
2026-06-29 23:30:45 -0500

    Update PrIntelligencePlatformApplication.java

--- src/main/java/com/jeshwin/printelligence/PrIntelligencePlatformApplication.java
+++ src/main/java/com/jeshwin/printelligence/PrIntelligencePlatformApplication.java
@@ -10,3 +10,4 @@ public class PrIntelligencePlatformApplication {
         SpringApplication.run(PrIntelligencePlatformApplication.class, args);
     }
 }
+                                             
